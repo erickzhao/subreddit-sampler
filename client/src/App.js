9028cd55a211e06fff3af8a04ea18a610ce72ea0
erick <erick@hotmail.ca>
2018-02-12 11:42:54 -0500

    Add authentication view

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchWrapper from './SearchWrapper';
+import Authenticator from './Authenticator';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    if (localStorage.isAuthenticated === undefined) {
+      localStorage.isAuthenticated = false;
+    }
+
+    this.state = {
+      isAuthenticated: JSON.parse(localStorage.isAuthenticated)
+    }
+  }
+
+  handleAuthenticate = () => {
+    fetch('/api/spotify')
+      .then(() => {
+        localStorage.isAuthenticated = JSON.stringify(true);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
+
   render() {
+    const { isAuthenticated } = this.state;
+
     return (
       <div className="App">
         <h1>fictional-octo-disco</h1>
-        <SearchWrapper/>
+        {
+          isAuthenticated ?
+          <SearchWrapper/> :
+          <Authenticator onAuthenticate={this.handleAuthenticate}/>
+        }
       </div>
     );
   }
---
+++ client/src/Authenticator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button';
+
+
+
+const Authenticator = ({onAuthenticate}) => {
+  return (
+    <div>
+      <Button onClick={onAuthenticate} label="Authenticate with Spotify"/>
+    </div>
+  )
+}
+
+export default Authenticator;
