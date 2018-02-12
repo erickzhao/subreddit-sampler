4e702f76cb3ab22fdbd39709ad6f1bb85ca394c2
erick <erick@hotmail.ca>
2018-02-12 01:10:18 -0500

    Display songs on subreddit fetch

--- client/src/ResultsWrapper.js
+++ client/src/ResultsWrapper.js
@@ -11,23 +11,39 @@ class ResultsWrapper extends Component {
   }
 
   componentDidMount() {
-    this.fetchTracks()
+    console.log(this.props.subreddit);
+    this.fetchTracks(this.props.subreddit)
       .then((res) => {
-        console.log(res);
-        this.setState({isLoaded: true});
+        this.setState({isLoaded: true, tracks: res});
       })
       .catch((err) => {
         console.error(err);
       });
   }
 
-  fetchTracks = () => {
-    return fetch(`/api/tracks`);
+  fetchTracks = (sub) => {
+    return fetch(`/api/r/${sub}`)
+    .then(res => res.json())
+    .catch((e) => {
+      console.error(e);
+    });
   }
 
   render() {
+    const { isLoaded, tracks } = this.state;
+
+    const trackList = tracks.map((t) =>
+      <li>
+        {t[0]} - {t[1]}
+      </li>
+    )
     return (
       <div>
+        {
+        !isLoaded &&
+          <h1>LOADING</h1>
+        }
+        <ul>{trackList}</ul>
       </div>
     )
   }
