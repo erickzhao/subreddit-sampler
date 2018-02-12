e2f9850131bf59035e6bcbc216975fa5d21bcc22
erick <erick@hotmail.ca>
2018-02-12 10:58:22 -0500

    Extract main search app into separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Search from './Search';
-import Return from './Return';
-import ResultsWrapper from './ResultsWrapper';
+import SearchWrapper from './SearchWrapper';
 
 class App extends Component {
-
-  // Initialize state
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      subreddit: null,
-      isSearched: false
-    }
-  }
-
-  handleSearch = (event) => {
-    this.setState({isSearched: true});
-    event.preventDefault();
-  }
-
-  handleChangeInSearch = (event) => {
-    this.setState({subreddit: event.target.value});
-  }
-
-  handleReturn = (event) => {
-    this.setState({subreddit: null, isSearched: false});
-  }
-
   render() {
-    const { subreddit } = this.state;
-
     return (
       <div className="App">
         <h1>fictional-octo-disco</h1>
-        {
-          !this.state.isSearched ? 
-          <Search subreddit={this.state.subreddit} onChange={this.handleChangeInSearch} onSearch={this.handleSearch}/>
-          :
-          <div>
-            <Return subreddit={this.state.subreddit} onReturn={this.handleReturn}/>
-            <ResultsWrapper subreddit={this.state.subreddit}/>
-          </div>
-        }
+        <SearchWrapper/>
       </div>
     );
   }
---
+++ client/src/SearchWrapper.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Search from './Search';
+import Return from './Return';
+import ResultsWrapper from './ResultsWrapper';
+
+class SearchWrapper extends Component {
+  // Initialize state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subreddit: null,
+      isSearched: false
+    }
+  }
+
+  handleSearch = (event) => {
+    this.setState({ isSearched: true });
+    event.preventDefault();
+  }
+
+  handleChangeInSearch = (event) => {
+    this.setState({ subreddit: event.target.value });
+  }
+
+  handleReturn = (event) => {
+    this.setState({ subreddit: null, isSearched: false });
+  }
+
+  render() {
+    const { subreddit, isSearched } = this.state;
+
+    return (
+      <div>
+        {
+          !isSearched ? 
+          <Search subreddit={subreddit} onChange={this.handleChangeInSearch} onSearch={this.handleSearch}/>
+          :
+          <div>
+            <Return subreddit={subreddit} onReturn={this.handleReturn}/>
+            <ResultsWrapper subreddit={subreddit}/>
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+export default SearchWrapper;
